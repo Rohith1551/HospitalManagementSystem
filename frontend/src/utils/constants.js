ed873c5d016aeed4032faff08bd4e4a8c7a4b9cd
Rohith R <rohith15022005@gmail.com>
2026-03-22 03:06:36 +0530

    ypadte

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,5 @@
 /** Backend base URL (Spring Boot runs on 8090) */
-export const API_BASE_URL = 'http://localhost:8090'
-
+export const API_BASE_URL = "http://100.53.137.254:8090";
 /** LocalStorage keys */
 export const TOKEN_KEY = 'hms_token'
 export const USER_KEY  = 'hms_user'
