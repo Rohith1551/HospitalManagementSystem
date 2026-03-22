03443802351610afa6ca471dee085db139642378
Rohith R <rohith15022005@gmail.com>
2026-03-23 01:27:51 +0530

    updated some cos

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 /** Backend base URL (Spring Boot runs on 8090) */
-export const API_BASE_URL = "http://50.19.182.65:8090";
+export const API_BASE_URL = "http://3.95.38.125:8090";
 /** LocalStorage keys */
 export const TOKEN_KEY = 'hms_token'
 export const USER_KEY  = 'hms_user'
